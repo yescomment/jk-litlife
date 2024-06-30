ec54cada9eae8f798939ed5e2cd6e5b38619af26
Jacob Ford <j@cobford.com>
2024-06-30 00:43:53 -0400

    siteURL debug

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,16 @@ let siteUrl = null
 
 if (process?.env?.SITE_URL) {
 	siteUrl = process.env.SITE_URL
+	console.log('siteUrl from', process.env.SITE_URL)
 } else if (process?.env?.VERCEL_PROJECT_PRODUCTION_URL) {
 	siteUrl = `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+	console.log('siteUrl from', process.env.VERCEL_PROJECT_PRODUCTION_URL)
 } else if (process?.env?.VERCEL_URL) {
 	siteUrl = `https://${process.env.VERCEL_URL}`
+	console.log('siteUrl from', process.env.VERCEL_URL)
 }
+
+console.log('siteUrl', siteUrl)
 
 // https://astro.build/config
 export default defineConfig({
