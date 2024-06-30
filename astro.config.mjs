2e078f8b8573a06a1f3ba5d59b6e5fbec239ad3d
Jacob Ford <j@cobford.com>
2024-06-30 00:30:52 -0400

    Add site URL, which should fix Vercel Toolbar on Staging

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import vercel from '@astrojs/vercel/static'
 // https://astro.build/config
 export default defineConfig({
 	output: 'static',
+	site: process?.env?.SITE_URL || process?.env?.VERCEL_PROJECT_PRODUCTION_URL || process?.env?.VERCEL_PROJECT_PRODUCTION_URL,
 	adapter: vercel({
 		imageService: true,
 		devImageService: true,
