173be2d6df21e3b63bef090a01590cf2fd55aed1
Jacob Ford <j@cobford.com>
2024-06-30 00:35:42 -0400

    Better site URL management

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,20 @@
 import { defineConfig } from 'astro/config'
 import vercel from '@astrojs/vercel/static'
 
+let siteUrl = null
+
+if (process?.env?.SITE_URL) {
+	siteUrl = process.env.SITE_URL
+} else if (process?.env?.VERCEL_PROJECT_PRODUCTION_URL) {
+	siteUrl = `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+} else if (process?.env?.VERCEL_URL) {
+	siteUrl = `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+}
+
 // https://astro.build/config
 export default defineConfig({
 	output: 'static',
-	site: process?.env?.SITE_URL || process?.env?.VERCEL_PROJECT_PRODUCTION_URL || process?.env?.VERCEL_PROJECT_PRODUCTION_URL,
+	site: siteUrl,
 	adapter: vercel({
 		imageService: true,
 		devImageService: true,
